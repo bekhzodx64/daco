8fdf8ed107a0e74dfd2021cf4e6a89e685f9813c
bekhzod tuychiev <toychiyevb@oks.uz>
2022-12-27 17:52:20 +0500

    footer fixed

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,6 +1,8 @@
 import { Montserrat } from "@next/font/google";
 import FooterBase from "./FooterBase";
 import FooterMain from "./FooterMain";
+import Image from "next/image";
+import background from "../../public/footer/background.jpg";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -9,9 +11,20 @@ const montserrat = Montserrat({
 
 const Footer = () => {
   return (
-    <div className={`${montserrat.variable} font-sans container mt-auto`}>
-      <FooterMain />
-      <FooterBase />
+    <div className={`${montserrat.variable} font-sans mt-auto relative pt-48`}>
+      <div className="absolute inset-0 bg-bgBlack -z-10">
+        <Image
+          src={background}
+          alt="background"
+          className="object-cover w-full h-full opacity-5"
+          draggable={false}
+        />
+      </div>
+
+      <div className="container">
+        <FooterMain />
+        <FooterBase />
+      </div>
     </div>
   );
 };
