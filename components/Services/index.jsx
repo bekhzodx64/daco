b32611b1dc8a9bd22d14b46c1e98f3392312eaa5
bekhzod tuychiev <toychiyevb@oks.uz>
2022-12-27 20:15:34 +0500

    services cards interactive

--- components/Services/index.jsx
+++ components/Services/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Montserrat } from "@next/font/google";
 import Image from "next/image";
 
@@ -10,36 +11,56 @@ const services = [
   {
     id: 1,
     title: "Design constructions",
+    description:
+      "We provide a single point of contact, saving you both time and money. Our professional team is supported by dedicated engineering resources so we can offer the following services:",
     icon: "/icons/services/1.svg",
   },
   {
     id: 2,
     title: "Installation of engineering network",
+    description:
+      "We provide a single point of contact, saving you both time and money. Our professional team is supported by dedicated engineering resources so we can offer the following services:",
     icon: "/icons/services/2.svg",
   },
   {
     id: 3,
     title: "Construction works ",
+    description:
+      "We provide a single point of contact, saving you both time and money. Our professional team is supported by dedicated engineering resources so we can offer the following services:",
     icon: "/icons/services/3.svg",
   },
   {
     id: 4,
     title: "Interior finishing works",
+    description:
+      "We provide a single point of contact, saving you both time and money. Our professional team is supported by dedicated engineering resources so we can offer the following services:",
     icon: "/icons/services/4.svg",
   },
   {
     id: 5,
     title: "Commissioning of equipment",
+    description:
+      "We provide a single point of contact, saving you both time and money. Our professional team is supported by dedicated engineering resources so we can offer the following services:",
     icon: "/icons/services/5.svg",
   },
   {
     id: 6,
     title: "Video surveilliance of during construction",
+    description:
+      "We provide a single point of contact, saving you both time and money. Our professional team is supported by dedicated engineering resources so we can offer the following services:",
     icon: "/icons/services/6.svg",
   },
 ];
 
 const Services = () => {
+  const [service, setService] = useState({
+    id: 1,
+    title: "Design constructions",
+    description:
+      "We provide a single point of contact, saving you both time and money. Our professional team is supported by dedicated engineering resources so we can offer the following services:",
+    icon: "/icons/services/1.svg",
+  });
+
   return (
     <div
       className={`${montserrat.variable} font-sans container space-y-14 pt-60`}
@@ -54,15 +75,12 @@ const Services = () => {
       <div className="flex flex-col items-center gap-10 lg:flex-row">
         <div>
           <div className="px-10 rounded-[20px] accent-gradient py-11 max-w-xs mx-auto">
-            <h3 className="text-2xl font-semibold">
-              We offer our high quality services
+            <h3 className="text-2xl font-semibold line-clamp-2">
+              {service.title}
             </h3>
-            <p className="text-[15px] opacity-60 mt-4">
-              We provide a single point of contact, saving you both time and
-              money. Our professional team is supported by dedicated engineering
-              resources so we can offer the following services:
+            <p className="text-[15px] opacity-60 mt-4 line-clamp-6">
+              {service.description}
             </p>
-
             <button
               type="button"
               className="text-[15px] flex items-center w-full justify-between border p-5 rounded-full mt-8"
@@ -79,19 +97,32 @@ const Services = () => {
         </div>
 
         <div className="grid items-start flex-1 w-full grid-cols-2 gap-4 md:gap-8 md:grid-cols-3 place-items-center h-min">
-          {services.map((service) => (
-            <div className="max-w-[215px]" key={service.id}>
+          {services.map((item) => (
+            <div
+              className="max-w-[215px] cursor-pointer"
+              key={item.id}
+              onClick={() => setService(item)}
+            >
               <div className="flex flex-col gap-5 text-center">
-                <div className="border w-[86px] h-[86px] flex items-center justify-center mx-auto rounded-2xl">
+                <div
+                  className={`border w-[86px] h-[86px] flex items-center justify-center mx-auto rounded-2xl ${
+                    service.id === item.id ? "active-service" : null
+                  }`}
+                >
                   <Image
-                    src={`${service.icon}`}
+                    src={`${item.icon}`}
                     width={40}
                     height={40}
                     alt="services"
+                    className={
+                      service.id === item.id
+                        ? "grayscale mix-blend-color-dodge"
+                        : null
+                    }
                   />
                 </div>
                 <p className="leading-tight text-[15px] md:text-lg">
-                  {service.title}
+                  {item.title}
                 </p>
               </div>
             </div>
